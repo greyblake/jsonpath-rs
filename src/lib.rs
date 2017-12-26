69ef2c4224bdf40fce19ddba76e6a307cd0e0bcb
Sergey Potapov <blake131313@gmail.com>
2017-12-26 23:09:11 +0100

    Initial commit; implement basic tokenizer

---
+++ src/lib.rs
@@ -0,0 +1,31 @@
+extern crate serde;
+extern crate serde_json;
+
+use serde_json::Value;
+
+mod tokenizer;
+
+
+
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    #[test]
+    fn it_works() {
+
+        let json = r#"
+            {
+                "name": "John",
+                "user": {
+                    "name": "greyblake",
+                    "age": 27
+                }
+            }
+        "#;
+
+        let v: Value = serde_json::from_str(json).unwrap();
+
+        println!("{:?}", v);
+    }
+}
