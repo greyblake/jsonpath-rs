3955677afc205509a3bc51a426696a80df4d3399
Sergey Potapov <blake131313@gmail.com>
2017-12-29 00:31:41 +0100

    Update README

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# JSONPath implementation for Rust
+# JSONPath for Rust
 
 The library is in hard development stage.
 
@@ -6,22 +6,45 @@ The library is in hard development stage.
 ## Example
 
 ```rust
-use jsonapth::Selector;
-use serde_json;
+extern crate jsonpath;
+extern crate serde_json;
+
+use jsonpath::Selector;
 use serde_json::Value;
 
-let selector = Selector::new("$.inventor.last_name").unwrap();
-let jsondoc = r#"
-    {
-        "inventor": {
-            "last_name": "Tesla"
+fn main() {
+    let jsondoc = r#"
+        {
+            "favorites": {
+               "books": [
+                   {
+                       "title": "Der schwarze Obelist",
+                       "author": "Erich Maria Remarque"
+                   },
+                   {
+                       "title": "Le mur",
+                       "author": "Jean-Paul Sartre"
+                   }
+               ]
+            }
         }
-    }
-"#;
-let json: Value = serde_json::from_str(jsondoc);
+    "#;
 
-assert_eq!(
-    selector.find(json),
-    Some(&Value::String("Tesla".to_owned()))
-)
+    // Parse JSON document
+    let json: Value = serde_json::from_str(jsondoc).unwrap();
+
+    // Create a JSONPath selector
+    let selector = Selector::new("$.favorites..title").unwrap();
+
+    // Apply the selector to the JSON and convert Vec<&Value> into Vec<&str>
+    let titles: Vec<&str> = selector.find_all(&json)
+        .iter()
+        .map(|t| t.as_str().unwrap())
+        .collect();
+    assert_eq!(titles, vec!["Der schwarze Obelist", "Le mur"]);
+}
 ```
+
+## Contributors
+
+- [greyblake](https://github.com/greyblake) Potapov Sergey - creator, maintainer.
--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,59 @@
+//! JSONPath implementation for Rust.
+//!
+//! The library is in development stage at the moment, but you can use already
+//! limited functionality.
+//!
+//! Currently the following operators are supported:
+//!
+//! * `$` - The root element. All path expression should start with it.
+//! * `.` - A direct child element.
+//! * `..` - Any descendant element (aka deep child).
+//!
+//!
+//! # Example
+//!
+//! ```
+//! extern crate jsonpath;
+//! extern crate serde_json;
+//!
+//! use jsonpath::Selector;
+//! use serde_json::Value;
+//!
+//! fn main() {
+//!     let jsondoc = r#"
+//!         {
+//!             "favorites": {
+//!                "books": [
+//!                    {
+//!                        "title": "Der schwarze Obelist",
+//!                        "author": "Erich Maria Remarque"
+//!                    },
+//!                    {
+//!                        "title": "Le mur",
+//!                        "author": "Jean-Paul Sartre"
+//!                    }
+//!                ]
+//!             }
+//!         }
+//!     "#;
+//!
+//!     // Parse JSON document
+//!     let json: Value = serde_json::from_str(jsondoc).unwrap();
+//!
+//!     // Create a JSONPath selector
+//!     let selector = Selector::new("$.favorites..title").unwrap();
+//!
+//!     // Apply the selector to the JSON and convert Vec<&Value> into Vec<&str>
+//!     let titles: Vec<&str> = selector.find_all(&json)
+//!         .iter()
+//!         .map(|t| t.as_str().unwrap())
+//!         .collect();
+//!     assert_eq!(titles, vec!["Der schwarze Obelist", "Le mur"]);
+//! }
+//!
+//! ```
+
+
 extern crate serde;
 extern crate serde_json;
 #[macro_use]
